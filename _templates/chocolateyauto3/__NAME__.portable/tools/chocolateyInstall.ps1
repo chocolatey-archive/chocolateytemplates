d8a0b4d3f0beaaaec22b29cf3960b559c8e83b6e
TomOne <TomOne@users.noreply.github.com>
2014-08-16 16:09:54 +0200

    Use Get-BinRoot function instead doing it manually

--- _templates/chocolateyauto3/__NAME__.portable/tools/chocolateyInstall.ps1
+++ _templates/chocolateyauto3/__NAME__.portable/tools/chocolateyInstall.ps1
@@ -12,12 +12,10 @@ $packageName = '__NAME__.portable' # arbitrary name for the package, used in mes
 $url = '{{DownloadUrl}}' # download url
 $url64 = '{{DownloadUrlx64}}' # 64bit URL here or remove - if installer decides, then use $url
 
-try { 
-  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+try {
+  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   ### For BinRoot, use the following instead ###
-  #$binRoot = "$env:systemdrive\tools"
-  ### Using an environment variable to to define the bin root until we implement configuration ###
-  #if($env:chocolatey_bin_root -ne $null -and $env:chocolatey_bin_root -notlike '*:\*'){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
+  #$binRoot = Get-BinRoot
   #$installDir = Join-Path $binRoot "$packageName"
   #Write-Host "Adding `'$installDir`' to the path and the current shell path"
   #Install-ChocolateyPath "$installDir"
@@ -29,7 +27,7 @@ try {
   ### OR for 7z ###
 
   # if (![System.IO.Directory]::Exists($installDir)) {[System.IO.Directory]::CreateDirectory($installDir)}
-  
+
   # $tempDir = "$env:TEMP\chocolatey\$($packageName)"
   # if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
 
@@ -41,5 +39,5 @@ try {
   Write-ChocolateySuccess "$packageName"
 } catch {
   Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw 
+  throw
 }
